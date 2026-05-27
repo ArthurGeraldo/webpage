0552e41a3131f28ec3fd546c19f396e97b8e53b8
ArthurGeraldo <arthurgeraldo718@gmail.com>
2026-05-27 16:57:07 -0300

    Teste de direcionamneto enter funcionando no login login atual: ashen - 123 redirecionamento pra outro html(da mesma pasta msm) botao de logout pro repositorio do git

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const botao = document.getElementById("botao");
 
 botao.addEventListener("click", function () {
 
-    const usuario = document.getElementById("user").value;
-    const senha = document.getElementById("pass").value;
+    const user = document.getElementById("user").value;
+    const pass = document.getElementById("pass").value;
 
-    if (usuario === "ash" && senha === "ash") {
+    if (user === "ashen" && pass === "123") {
 
-        window.location.href = "https://i.pinimg.com/736x/8d/36/b8/8d36b87c0c2ac5af274647a06484a45f.jpg"; /*teste*/
-
-    } else {
-
-        alert("Usuário ou senha incorretos.");
+        window.location.href = "./index2.html";
 
     }
+    else {
+        alert("Login ou senha incorreta");
+        location.reload();
+    }
+});
+document.addEventListener("keypress", function(event){
+
+    if(event.key === "Enter"){
+        botao.click();
+    }
+
 });
---
+++ index2.js
@@ -0,0 +1,5 @@
+const logout = document.getElementById("logout");
+
+logout.addEventListener("click", function () {
+    window.location.href = "https://arthurgeraldo.github.io/webpage/";
+})
